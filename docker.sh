492215e3684f925c2982dedfe258765c7e786094
renve <42286510+renve@users.noreply.github.com>
2019-04-29 14:46:45 -0400

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -28,4 +28,13 @@ sudo apt-get update
 echo "installing docker"
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
+#Install link for docker-machine
+#https://docs.docker.com/machine/install-machine/
+base=https://github.com/docker/machine/releases/download/v0.16.0 &&
+  curl -L $base/docker-machine-$(uname -s)-$(uname -m) >/tmp/docker-machine &&
+  sudo install /tmp/docker-machine /usr/local/bin/docker-machine
+
+#Install docker compose and add executable
+sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 
