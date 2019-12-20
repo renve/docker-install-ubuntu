35fd83aba51b226282b71b6ec13a625fb8e26611
renve <jventi49@gmail.com>
2019-12-20 00:10:34 +0000

    testing detached head on MASTER

--- docker.sh
+++ docker.sh
@@ -50,3 +50,4 @@ sudo reboot now
 #sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 #sudo chmod +x /usr/local/bin/docker-compose
 
+####TESTING DETACH HEAD FIX###
